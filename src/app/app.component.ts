d547887ca18c8a2512917ca283ee746d1bfa6efa
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-02-29 10:37:58 -0700

    Cambios tercer dia
    Se agrego el tercer componente y se agregaron las rutas en la parte de routes.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { PrimerDiaComponent } from './primer-dia/primer-dia.component';
 import { SegundoDiaComponent } from './segundo-dia/segundo-dia.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PrimerDiaComponent,SegundoDiaComponent],
+  imports: [RouterOutlet,PrimerDiaComponent,SegundoDiaComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
